a2d9d1a0300f01421ee266185ec771e8405a88f5
Wolfe Styke <wstyke@mit.edu>
2012-10-05 19:25:10 -0700

    added getTabByName to model

--- ext/background/model.js
+++ ext/background/model.js
@@ -1,17 +1,19 @@
 /**
- * @filedesc Model for read/write of 
- * saved tab information in persistant storage.
+ * Model: Saved tab info with persistant storage.
  *
  * Author: Wolfe Styke - <wstyke@gmail.com>
  */
 
 var TS = TS || {};
+/**
+ * Namespace: model.
+ */
 TS.model = TS.model || {};
 
 
 /**
  * Return dict of tab names to saved tab info objects.
- * @returns {Object} tabdict Dict mapping name to tab info.
+ * @return {Object} tabdict Dict mapping name to tab info.
  */
 TS.model.getNamedTabs = function() {
   var tabdict = JSON.parse(localStorage.getItem('tabdict'));
@@ -22,26 +24,32 @@ TS.model.getNamedTabs = function() {
 };
 
 /**
+ * Nukes all tab info.
+ */
+TS.model.nukeAllTabs = function() {
+    TS.model.saveNamedTabs({});
+};
+
+/**
  * Save named tab dict into localStorage.
  * @param {Object} tabdict Dict mapping tab name to tab info object.
  */
 TS.model.saveNamedTabs = function(tabdict) {
-    localStorage.setItem('tabdict',
-			 JSON.stringify(tabdict));
+    localStorage.setItem('tabdict', JSON.stringify(tabdict));
 };
 
 
 /**
  * Add a new uniquely named tab to TS.model.
  * @param {Object} data Dict with tab name and tab info.
- * @returns {boolean} true if save worked.
+ * @return {boolean} true if save worked.
+ * @this TS.model
  */
 TS.model.addNamedTab = function(data) {
     var tabdict = this.getNamedTabs();
     if (tabdict[data.name] !== undefined) {
-			debug("TS.model.addNamedTab: Cannot add tab", data.name, 
-						", name already used.");
-			return false;
+        // Name already saved to a tab.
+        return false;
     }
     tabdict[data.name] = data;
     this.saveNamedTabs(tabdict);
@@ -49,13 +57,26 @@ TS.model.addNamedTab = function(data) {
 };
 
 /**
+ * Retrieve tab item by name.
+ * @param {string} tabName The name of desired tab.
+ * @return {object} tab Saved tab or undefined.
+ * @this TS.model
+ */
+TS.model.getTabByName = function(tabName) {
+    var tabdict = this.getNamedTabs();
+    return tabdict[tabName];
+};
+
+
+/**
  * Removes tab info for the specified named tab.
  * @param {string} tabName The name of the saved tab to delete.
+ * @this TS.model
  */
 TS.model.removeNamedTab = function(tabName) {
     var tabdict = this.getNamedTabs();
     if (name in tabdict) {
-	delete tabdict[name];
+        delete tabdict[name];
     }
     this.saveNamedTabs(tabdict);
 };
@@ -64,9 +85,7 @@ TS.model.removeNamedTab = function(tabName) {
 
 
 /**
- * 
- * @param {}
- * @returns {}
+ * Unused.
  */
 TS.model.a = function() {
 
@@ -74,9 +93,7 @@ TS.model.a = function() {
 
 
 /**
- * 
- * @param {}
- * @returns {}
+ * Unused.
  */
 TS.model.a = function() {
 
