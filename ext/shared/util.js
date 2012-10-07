910159c1beaaefa23075255db5fc436da6a043c9
Wolfe Styke <wstyke@mit.edu>
2012-10-06 23:09:45 -0700

    Added method to escape special RegExp chars in string: TS.util.escapeRegExp

--- ext/shared/util.js
+++ ext/shared/util.js
@@ -1,8 +1,42 @@
 var TS = TS || {};
+/**
+ * Utility Methods.
+ */
+TS.util = TS.util || {};
+
+/**
+ * Debug toggle boolean.
+ */
 TS.DEBUG = true;
 
 function debug() {
-  if (TS.DEBUG) {
-    console.log.apply(console, arguments);
-  }
+    // Only global function, for ease of typing.
+    if (TS.DEBUG) {
+        console.log.apply(console, arguments);
+    }
 }
+
+/**
+ * Returns true if urlString looks like a url.
+ * TODO(wstyke:10-06-12): Improve this with regexp.
+ * @param {string} urlString The string to check.
+ * @return {boolean} isUrl True if urlString is a url.
+ */
+TS.util.isUrl = function(urlString) {
+    var isUrl = false;
+    if (urlString.search('http://') === 0 ||
+            urlString.search('https://') === 0 ||
+            urlString.search('chrome://') === 0) {
+        return true;
+    }
+    return isUrl;
+};
+
+/**
+ * Escape RegExp special chars from a string.
+ * @param {string} text The unescaped string.
+ * @return {string} The string with escaped RegExp special chars.
+ */
+TS.util.escapeRegExp = function(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+};
