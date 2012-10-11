e08f865ce2cf729ab2fd54f35646b3628438aad7
Wolfe Styke <wstyke@mit.edu>
2012-10-10 17:58:15 -0700

    Cosmetic changes.

--- ext/background/model.js
+++ ext/background/model.js
@@ -104,7 +104,7 @@ TS.model.getTabsByFuzzyName = function(queryName) {
             for (var i = 0; i < tabFolders.length; i++) {
                 // Fuzzy Match within a folder name.
                 if (fuzzyNameRegExp.test(tabFolders[i])) {
-                    tab.rankOrder = 3;
+                    tab.rankOrder = 2;
                     tab.rankPos = i;
                     debug(4, tab.name, queryName);
                     break;
@@ -113,7 +113,7 @@ TS.model.getTabsByFuzzyName = function(queryName) {
             if (tab.rankPos === undefined &&
                     fuzzyNameRegExp.test(tab.name)) {
                 // Fuzzy Match.
-                tab.rankOrder = 4;
+                tab.rankOrder = 3;
                 tab.rankPos = tab.name.search(queryName[0]);
                 debug(5, tab.name, queryName);
             }
