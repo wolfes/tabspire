80a1c2dbe3388102612dd4456412793897120cc9
Wolfe Styke <wstyke@mit.edu>
2012-10-13 01:47:32 -0700

    Added getMatchesByFuzzyName to TS.dbUtil

---
+++ ext/background/db_util.js
@@ -0,0 +1,79 @@
+/**
+ * Util Methods for DB.
+ *
+ */
+
+/**
+ * Global Project Namespace.
+ */
+var TS = TS || {};
+/**
+ * Database Utility Namespace.
+ */
+TS.dbUtil = TS.dbUtil || {};
+
+/**
+ * Given objects having names, return items matching queryName.
+ * @param {object} itemDict Maps names to items.
+ * @param {string} queryName The name of the desired match.
+ * @return {array} matches The list of matches.
+ */
+TS.dbUtil.getMatchesByFuzzyName = function(itemDict, queryName) {
+    var matches = [];
+    if (queryName === undefined) {
+        for (var name in itemDict) {
+            matches.push(itemDict[name]);
+        }
+        return matches;
+    }
+    for (var name in itemDict) {
+        var match = itemDict[name];
+        // Condense first 3 cases:
+        // rankOrder = 1, rankPos = -10, 0, 1+
+        if (match.name.search(queryName) !== -1) {
+            match.rankOrder = 1;
+            // Rank = Position of queryName in the match's name.
+            // Lower is better
+            match.rankPos = match.name.search(queryName);
+            if (match.name === queryName) {
+                match.rankPos = -10; // Sort Exact Match to front.
+            }
+            debug(1, match.rankPos, match.name);
+        } else {
+            // Fuzzy Match: by folders, then by entire string.
+            // Redundent folder matching with query anywhere?
+            escMatchName = TS.util.escapeRegExp(queryName);
+            matchFolders = match.name.split('/');
+            fuzzyNameRegExp = new RegExp(
+                    escMatchName.split('').join('.*'));
+            for (var i = 0; i < matchFolders.length; i++) {
+                // Fuzzy Match within a folder name.
+                if (fuzzyNameRegExp.test(matchFolders[i])) {
+                    match.rankOrder = 2;
+                    match.rankPos = i;
+                    debug(4, match.name, queryName);
+                    break;
+                }
+            }
+            if (match.rankPos === undefined &&
+                    fuzzyNameRegExp.test(match.name)) {
+                // Fuzzy Match.
+                match.rankOrder = 3;
+                match.rankPos = match.name.search(queryName[0]);
+                debug(5, match.name, queryName);
+            }
+        }
+        if (match.rankOrder !== undefined && match.rankPos !== undefined) {
+            matches.push(match);
+        }
+    }
+    // Order matches by rank: Order then Position.
+    function sortMatches(a, b) {
+        return ((a.rankOrder !== b.rankOrder) ?
+                a.rankOrder > b.rankOrder :
+                a.rankPos > b.rankPos);
+    }
+    matches.sort(sortMatches);
+    return matches;
+};
+
